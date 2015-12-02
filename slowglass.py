6c6cd61674a52ba96f71544bc14bac1869b4718d
Greenwald, David (ELS-DAY) <david.greenwald@lexisnexis.com>
2015-12-02 16:07:02 -0500

    created code to start both halves and cleanup and startup and shutdown

--- slowglass.py
+++ slowglass.py
@@ -5,38 +5,47 @@ import time
 from datetime import datetime
 import re
 
-steadyStateFPS = 10
-desiredBuffer = 60*60 #1 minute * 60 seconds
-numberOfFrames = steadyStateFPS*desiredBuffer;
-fmt = '%Y-%m-%d %H:%M:%S'
-
-disp = Display()
-
-filelist = []
-frameCounter = 101
-sleepTime = .1
-
-while disp.isNotDone():
-    if frameCounter > 100 or len(filelist) == 0:
-        frameCounter = 0
-        filelist = glob("images/*.jpg")
-        if len(filelist)>numberOfFrames:
-            sleepTime = 1.0/steadyStateFPS
-            print "number of frames in buffer="+str(len(filelist))+" desired="+str(numberOfFrames)+" setting sleeptime to "+str(sleepTime)
-        else:
-            sleepTime = (1.0/steadyStateFPS)+.01
-            print "number of frames in buffer="+str(len(filelist))+" desired="+str(numberOfFrames)+" setting sleeptime to "+str(sleepTime)
 
 
-    filename = filelist.pop(0)
-    img = Image(filename)
-    matchObj = re.search(r'[0-9- :]+', filename)
+def Run(cmdPipe):
+    steadyStateFPS = 10
+    desiredBuffer = 60*60 #1 minute * 60 seconds
+    numberOfFrames = steadyStateFPS*desiredBuffer;
+    fmt = '%Y-%m-%d %H:%M:%S'
 
-    d1_ts = time.mktime(datetime.strptime(matchObj.group(), fmt).timetuple())
-    d2_ts = time.mktime(datetime.utcnow().timetuple())
-    offset = int(d1_ts-d2_ts)/60
-    img.drawText(str(offset),  x=600, y=470)
-    img.save(disp)
-    os.remove(filename)
-    frameCounter = frameCounter+1
-    time.sleep(sleepTime)
+    disp = Display()
+
+    filelist = []
+    frameCounter = 101
+    sleepTime = .1
+
+    while disp.isNotDone():
+        # check command
+        if cmdPipe.poll():
+            cmd = cmdPipe.recv()
+            if cmd=='shutdown':
+                break
+
+        if frameCounter > 100 or len(filelist) == 0:
+            frameCounter = 0
+            filelist = glob("images/*.jpg")
+            if len(filelist)>numberOfFrames:
+                sleepTime = 1.0/steadyStateFPS
+                print("player", 0, "number of frames in buffer="+str(len(filelist))+" desired="+str(numberOfFrames)+" setting sleeptime to "+str(sleepTime))
+            else:
+                sleepTime = (1.0/steadyStateFPS)+.01
+                print("player", 0, "number of frames in buffer="+str(len(filelist))+" desired="+str(numberOfFrames)+" setting sleeptime to "+str(sleepTime))
+
+
+        filename = filelist.pop(0)
+        img = Image(filename)
+        matchObj = re.search(r'[0-9- :]+', filename)
+
+        d1_ts = time.mktime(datetime.strptime(matchObj.group(), fmt).timetuple())
+        d2_ts = time.mktime(datetime.utcnow().timetuple())
+        offset = int(d1_ts-d2_ts)/60
+        img.drawText(str(offset),  x=600, y=470)
+        img.save(disp)
+        os.remove(filename)
+        frameCounter = frameCounter+1
+        time.sleep(sleepTime)
