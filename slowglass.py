73f5e3a9a8fba831885b6994c21e5ab21b011e52
Greenwald, David (ELS-DAY) <david.greenwald@lexisnexis.com>
2015-12-02 16:09:08 -0500

    renamed pieces to better reflect application componants

--- slowglass.py
+++ slowglass.py
@@ -1,51 +1,93 @@
-from SimpleCV import Image, Display
-from glob import glob
+
+import sys, time
+from os import sep
+import socket
+from multiprocessing import Process, Pipe
+from multiprocessing.managers import BaseManager
+import signal, errno
+import shutil
 import os
-import time
-from datetime import datetime
-import re
 
+import capture, slowglass
 
+procs = {}
+pipes = {}
+running = False
 
-def Run(cmdPipe):
-    steadyStateFPS = 10
-    desiredBuffer = 60*60 #1 minute * 60 seconds
-    numberOfFrames = steadyStateFPS*desiredBuffer;
-    fmt = '%Y-%m-%d %H:%M:%S'
+def startup():
+    global procs
+    global pipes
+    global running
 
-    disp = Display()
+    #cleanup in case of bad shutdown
+    shutil.rmtree('images', ignore_errors=True)
+    os.mkdir("images")
 
-    filelist = []
-    frameCounter = 101
-    sleepTime = .1
+    running = True
 
-    while disp.isNotDone():
-        # check command
-        if cmdPipe.poll():
-            cmd = cmdPipe.recv()
-            if cmd=='shutdown':
-                break
+    # init capture
+    master, slave = Pipe()  # endpoint [0]-PlexConnect, [1]-DNSServer
+    proc = Process(target=capture.Run, args=[slave])
+    proc.start()
 
-        if frameCounter > 100 or len(filelist) == 0:
-            frameCounter = 0
-            filelist = glob("images/*.jpg")
-            if len(filelist)>numberOfFrames:
-                sleepTime = 1.0/steadyStateFPS
-                print("player", 0, "number of frames in buffer="+str(len(filelist))+" desired="+str(numberOfFrames)+" setting sleeptime to "+str(sleepTime))
-            else:
-                sleepTime = (1.0/steadyStateFPS)+.01
-                print("player", 0, "number of frames in buffer="+str(len(filelist))+" desired="+str(numberOfFrames)+" setting sleeptime to "+str(sleepTime))
+    time.sleep(0.1)
+    if proc.is_alive():
+        procs['capture'] = proc
+        pipes['capture'] = master
+    else:
+        print('Slowglass', 0, "capture not alive. Shutting down.")
+        running = False
 
+    time.sleep(2)
+    # init player
+    if running:
+        master, slave = Pipe()  # endpoint [0]-PlexConnect, [1]-WebServer
+        proc = Process(target=player.Run, args=[slave])
+        proc.start()
 
-        filename = filelist.pop(0)
-        img = Image(filename)
-        matchObj = re.search(r'[0-9- :]+', filename)
+        time.sleep(0.1)
+        if proc.is_alive():
+            procs['player'] = proc
+            pipes['player'] = master
+        else:
+            dprint('Slowglass', 0, "player not alive. Shutting down.")
+            running = False
 
-        d1_ts = time.mktime(datetime.strptime(matchObj.group(), fmt).timetuple())
-        d2_ts = time.mktime(datetime.utcnow().timetuple())
-        offset = int(d1_ts-d2_ts)/60
-        img.drawText(str(offset),  x=600, y=470)
-        img.save(disp)
-        os.remove(filename)
-        frameCounter = frameCounter+1
-        time.sleep(sleepTime)
+    # not started successful - clean up
+    if not running:
+        cmdShutdown()
+        shutdown
+
+def shutdown():
+    for slave in procs:
+        procs[slave].join()
+    shutil.rmtree('images')
+    print('Slowglass', 0, "shutdown")
+
+def cmdShutdown():
+    global running
+    running = False
+    # send shutdown to all pipes
+    for slave in pipes:
+        pipes[slave].send('shutdown')
+    print('Slowglass', 0, "Shutting down.")
+
+def sighandler_shutdown(signum, frame):
+    signal.signal(signal.SIGINT, signal.SIG_IGN)  # we heard you!
+    cmdShutdown()
+
+if __name__=="__main__":
+    signal.signal(signal.SIGINT, sighandler_shutdown)
+    signal.signal(signal.SIGTERM, sighandler_shutdown)
+
+    print('Slowglass', 0, "***")
+    print('Slowglass', 0, "Slowglass")
+    print('Slowglass', 0, "Press CTRL-C to shut down.")
+    print('Slowglass', 0, "***")
+
+    running = startup()
+
+    while running:
+        running = run()
+
+    shutdown()
